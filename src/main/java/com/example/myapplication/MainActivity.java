da9d22964457a82b15baf66f09988d26392e2c55
Aspa7beginner <65359456+Aspa7beginner@users.noreply.github.com>
2023-02-16 12:50:30 +0200

    Update MainActivity.java

--- src/main/java/com/example/myapplication/MainActivity.java
+++ src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,30 @@
 package com.example.myapplication;
 
-/**
+/*
+
+ * The user can input sequences of numbers for three functions factorial, fibonacci and gcd
  *
- * The user can input sequences of number for three function factorial, fibonacci and gcd
+ * The user can press a function button and give different inputs. What happens is:
  *
- * The user can press a function button and give different inputs, what happens is:
+ * After the button is pressed, each sequence of numbers are are fed to a thread 
+ * that calculates the result in the background. Each time the result of a sequence is ready
+ * the results are displayed on screen.
  *
- * each sequence of numbers after the button is pressed are stored as a string in a list
- * a thread runs in the background and processes the sequences each at at time and when ready
- * the results are displayed on screed.
- *
- * the above process happens for all functions, the press of a button for one does not affect
+ * The above process happens for all functions, the press of a button for one does not affect
  * the others.
  *
- * One can add different delays in thread result 'delivery' time, to see the threads work
- * in action. The delay can be modified in lines 90 to 99 for each loader separately.
+ * One can pass different delay numbers to threads, to see them work
+ * in action. The delay can be modified in lines 77 to 81 for each function separately.
  *
  */
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
+
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Bundle;;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -31,21 +33,15 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.LinkedList;
+import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Bundle>{
+public class MainActivity extends AppCompatActivity{
 
-    // User input validation must be comma separated numbers (integers)
-    private final String INPUT_VALIDATION_PATTERN = " *(?:(?:\\d+ *,)+ *)*(?:\\d+ *)";
+    // prev regex" *(?:(?:\\d{1,3} *,)+ *)*(?:\\d{1,3} *)";
     private LinearLayout parentResultsLinearLayout;
     private EditText parameters;
     LayoutInflater inflater;
-
-
-    // Structures used caching user inputs (used as FIFO queues)
-    LinkedList<String> inputsCashFibonacci = new LinkedList<String>();
-    LinkedList<String> inputsCashGCD = new LinkedList<String>();
-    LinkedList<String> inputsCashFactorial = new LinkedList<String>();
 
 
     @Override
@@ -78,106 +74,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // a reference to the appropriate function's cache
         // and its loader's id (dedicated thread for that function)
 
-        factorialButton.setOnClickListener(view -> onFunctionChooseListener(inputsCashFactorial, 0));
+        factorialButton.setOnClickListener(view -> onFunctionChooseListener("factorial","8"));
 
-        gcdButton.setOnClickListener(view -> onFunctionChooseListener(inputsCashGCD, 1));
+        gcdButton.setOnClickListener(view -> onFunctionChooseListener("gcd", "10"));
 
-        fibonacciButton.setOnClickListener(view -> onFunctionChooseListener(inputsCashFibonacci, 2));
+        fibonacciButton.setOnClickListener(view -> onFunctionChooseListener("fibonacci", "4"));
     }
 
-
-
-    // here new threads with the appropriate arguments are created be Loader Manager
-    // inputs reference to cache, mode-function and delay value for the sleep function
-    @Override
-    public Loader<Bundle> onCreateLoader(int id, Bundle args) {
-        switch (id)
-        {
-            case 0:
-                return new CalculationLoader(this,args, inputsCashFactorial, "factorial", 1);
-            case 1:
-                return new CalculationLoader(this, args, inputsCashGCD, "gcd", 1);
-            case 2:
-                return new CalculationLoader(this, args, inputsCashFibonacci, "fibonacci",1);
-            default:
-                return null;
-        }
-    }
-
-    // this function manages what happens after the threads have finished their job
-    @Override
-    public void onLoadFinished(Loader<Bundle> loader, Bundle data) {
-        // in case of no data returned do nothing
-        if (data == null)
-        {
-            return;
-        }
-
-        // create the view for displaying the results,
-        int createdViewId = addFunctionResultsView();
-
-        // take reference to that view
-        TextView v = (TextView) findViewById(createdViewId);
-
-        if (v != null)
-        {
-            // in case all went well attach the result in v
-            v.setText(data.getString("result"));
-        }
-
-
-        // detect which loader sent the results (commenting added
-        // for the 1st loader, the same applies to the others
-
-        switch (loader.getId())
-        {
-            case 0:
-                // remove last value from cache (user's first input)
-                inputsCashFactorial.removeLast();
-
-                // if the cache contains user input, make the thread
-                // start loading again (the last value in cache)
-                if(inputsCashFactorial.size() != 0)
-                {
-                    loader.startLoading();
-                } else
-                {
-                    // otherwise kill the loader, its work is done
-                    LoaderManager.getInstance(MainActivity.this).destroyLoader(0);
-                }
-                break;
-
-            case 1:
-                inputsCashGCD.removeLast();
-                if(inputsCashGCD.size() != 0)
-                {
-                    loader.startLoading();
-                } else
-                {
-                    LoaderManager.getInstance(MainActivity.this).destroyLoader(1);
-                }
-                break;
-
-            case 2:
-                inputsCashFibonacci.removeLast();
-                if(inputsCashFibonacci.size() != 0)
-                {
-                    loader.startLoading();
-                } else
-                {
-                    LoaderManager.getInstance(MainActivity.this).destroyLoader(2);
-                }
-                break;
-            default:
-                break;
-        }
-    }
-
-
-    @Override
-    public void onLoaderReset(Loader<Bundle> loader) { ;
-        //TODO: default stub not implemented
-    }
 
 
 
@@ -196,31 +99,162 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     }
 
     // listener template
-    public void onFunctionChooseListener(LinkedList<String> cache, int loaderId){
+    public void onFunctionChooseListener(String mode, String delay){
         // user input
         String params = parameters.getText().toString().trim();
 
         // if there are not values given and there aren't comma separated numbers,
         // show toast and nothing else
-        if (params.length() == 0 && !params.matches(INPUT_VALIDATION_PATTERN))
+        // User input validation must be comma separated numbers (integers)
+        String INPUT_VALIDATION_PATTERN = "^(?:[\\d]{1,4},)+[^,]{1,4}$";
+        if (params.length() == 0 || !params.matches(INPUT_VALIDATION_PATTERN))
         {
-            Toast.makeText(getApplicationContext(), getString(R.string.invalid_input_message),
+            Log.i("In not matced", "Herre");
+            Toast.makeText(getBaseContext(), getString(R.string.invalid_input_message),
                     Toast.LENGTH_SHORT).show();
             return;
         }
 
-        //  if the cache is empty or the same input is not given
-        //  before the loader finishes its work
-        // then add input parameters to cache
-        if (cache.isEmpty() || !cache.getFirst().equals(params)){
-            cache.addFirst(params);
-        }
 
         // if the loader with loaderId already exists, replace previous callbacks
         // with new ones, otherwise create a loader with that id
-        LoaderManager.getInstance(MainActivity.this)
-                .initLoader(loaderId, null, MainActivity.this);
+        new CalculateTask().execute(mode, params, delay);
 
     }
 
+    // params String[] where 0th element is the function and 1st the number sequence to calculate
+    // also there is a last parameter for thread testing, it delays the computation by some seconds
+    // to see if the UI freezes
+    @SuppressLint("StaticFieldLeak")
+    private class CalculateTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String...params) {
+            StringBuilder result = new StringBuilder();
+
+            // redundant, just for testing
+            try {
+                TimeUnit.SECONDS.sleep(Integer.parseInt(params[2]));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (params[0].equals("gcd")) {
+                return gcd(params[1]);
+            }
+
+            for (String num: params[1].split(",")) {
+                if (params[0].equals("fibonacci")) {
+                    result.append(fibonacci(num.trim())).
+                            append(System.getProperty("line.separator"));
+                    continue;
+                }
+                result.append(factorial(num.trim())).
+                        append(System.getProperty("line.separator"));
+            }
+
+            return result.toString();
+
+        }
+
+        protected void onPostExecute(String result) {
+            // create the view for displaying the results,
+            int createdViewId = addFunctionResultsView();
+
+            // take reference to that view
+            TextView v = (TextView) findViewById(createdViewId);
+            v.setText(result);
+        }
+    }
+
+    /*
+     * Functions for calculation of Factorial, Fibonacci and greater common denominator
+     *
+     * Some notes:
+     * For the concatenation of the results, there was the use of a String Builder
+     * it is faster than string concatenation and more memory efficient (Mutable data type)
+     *
+     * Also the results of factorial and fibonacci functions are created as BigIntegers,
+     * since up to 20! or 50! even longs aren't enough.
+     *
+     * !!!Caution!!!
+     * The user shouldn't overdo it, otherwise the are stackoverflow crashes
+     * and as a result so does the app
+     *
+     */
+
+    private String factorial(String number) {
+        StringBuilder result = new StringBuilder();
+
+        BigInteger factorial = new BigInteger(number);
+        for (int i=2; i <= Long.parseLong(number); i++){
+            try {
+                factorial = factorial.multiply(BigInteger.valueOf(i));
+            } catch (StackOverflowError | OutOfMemoryError | NumberFormatException e) {
+                result.append(": Large Result Number: not enough memory!");
+            }
+        }
+
+        result.append(number).append( " != ").append(factorial);
+
+        return result.toString();
+    }
+
+    private String fibonacci(String number) {
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("(F").append(number).append(") = {");
+
+        BigInteger Fn_2 = BigInteger.valueOf(0);
+        result.append(Fn_2).append(",");
+
+        BigInteger Fn_1 = BigInteger.valueOf(1);
+        result.append(Fn_1).append(",");
+
+        String delimiter = "";
+        for (int j = 2; j < Integer.parseInt(number.trim()); j++) {
+            result.append(delimiter); //do not add delimiter as its added already
+            delimiter = ","; // then set the delimiter to add it for the next sequence number
+            try {
+                BigInteger Fn = Fn_1.add(Fn_2);
+                result.append(Fn);
+
+                Fn_2 = Fn_1;
+                Fn_1 = Fn;
+            } catch (StackOverflowError | OutOfMemoryError | NumberFormatException e) {
+                result.append(": Large Number: not enough memory!");
+            }
+        }
+        result.append("}");
+
+        return result.toString();
+    }
+
+    private String gcd(String numSequence) {
+        String[] numbers = numSequence.split(",");
+        StringBuilder result = new StringBuilder();
+        result.append("GCD(").append(numSequence).append(") = ");
+
+        Long gcd_all = Long.valueOf(numbers[0].trim());
+        for(int i =1; i< numbers.length; i++){
+
+            gcd_all = gcd(gcd_all, Long.valueOf(numbers[i].trim()));
+
+            // used to break the loop sooner
+            // (if a gcd of 1 is found before all numbers get examined)
+            if (gcd_all == 1) {
+                result.append(1);
+            }
+        }
+
+        result.append(gcd_all);
+
+        return result.toString();
+    }
+
+    // calculated based on the Euclidean Algorithm
+    private static Long gcd(Long a, Long b){
+        return a == b? a : gcd(b, Math.abs(a-b));
+    }
 }
